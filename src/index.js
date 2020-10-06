66806fad5d32f8118357f2bccfdc442eff0b04f7
Alex Wang <alex.wang@mpac.ca>
2020-10-06 16:34:39 -0400

    * Modernize greeting message feature * Resolve issue with calling props of undefined * Modernize passing sessionAttributes using useState hook * Add missing prop types * Add missing default prop values * Code Cleanup based on eslint feedback

--- src/index.js
+++ src/index.js
@@ -1,269 +1,255 @@
-import React, { Component } from "react";
+import React, { useEffect, useState, useRef } from "react";
+import { isEqual } from "lodash";
 import PropTypes from "prop-types";
 import merge from "lodash/merge";
-import "aws-sdk";
+import AWS from "aws-sdk";
 import "./styles/chatbot.css";
 
-class LexChat extends React.Component {
-  constructor(props) {
-    super(props);
+//stateful component
+function LexChat(props) {
+  AWS.config.region = props.region;
+  AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: props.IdentityPoolId,
+  });
+  let lexruntime = new AWS.LexRuntime();
+  let conversationDivRef = useRef(null);
+  // let greetingMsgRef = useRef(null);
 
-    this.state = {
-      data: "",
-      lexUserId: "chatbot" + Date.now(),
-      sessionAttributes: this.props.sessionAttributes,
-      visible: "closed",
-    };
-    this.conversationDivRef = React.createRef();
-    this.greetingMsgRef = React.createRef();
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+  //default states being set
+  const [state, setState] = useState({
+    data: "",
+    lexUserId: "chatbot" + Date.now(),
+    sessionAttributes: props.sessionAttributes,
+    visible: "closed",
+  });
 
-  componentDidMount() {
-    document.getElementById("inputField").focus();
+  //Sets configurations from AWS and sets identity poolId
 
-    let greetingNode = document.createElement("P");
-    this.greetingMsgRef.current = greetingNode;
-    greetingNode.className = "lexResponse";
-    greetingNode.appendChild(document.createTextNode(this.props.greeting));
-    greetingNode.appendChild(document.createElement("br"));
-    this.conversationDivRef.current.appendChild(greetingNode);
-
-    AWS.config.region = this.props.region || "us-east-1";
-    AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-      IdentityPoolId: this.props.IdentityPoolId,
-    });
-    var lexruntime = new AWS.LexRuntime();
-    this.lexruntime = lexruntime;
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      this.props.sessionAttributes &&
-      this.props.sessionAttributes !== prevState.sessionAttributes
-    ) {
-      this.state.sessionAttributes = {
-        ...this.state.sessionAttributes,
-        ...this.props.sessionAttributes,
-      };
+  //useEffect runs when the dom renders this component
+  useEffect(() => {
+    if (!isEqual(props.sessionAttributes, state.sessionAttributes)) {
+      setState({
+        ...state,
+        sessionAttributes: {
+          ...state.sessionAttributes,
+          ...props.sessionAttributes,
+        },
+      });
     }
+  }, [props.sessionAttributes, state]);
 
-    if (this.props.greeting && this.props.greeting !== prevProps.greeting) {
-      const greetingNodeRef = this.greetingMsgRef.current;
-      if (greetingNodeRef) {
-        greetingNodeRef.textContent = this.props.greeting;
-      }
-    }
-  }
-
-  handleClick() {
-    this.setState({
-      visible: this.state.visible == "open" ? "closed" : "open",
-    });
-    if (this.props.debugMode === true) {
-      console.log(this.state);
-    }
-  }
-
-  pushChat(event) {
+  // handling the changed value in input
+  function handleChange(event) {
     event.preventDefault();
+    setState({ ...state, data: event.target.value });
+  }
 
-    var inputFieldText = document.getElementById("inputField");
+  //handling the slide open or close of the chat
+  function handleClick(e) {
+    e.preventDefault();
+    setState((prevState) => ({
+      ...prevState,
+      visible: state.visible == "open" ? "closed" : "open",
+    }));
+    if (props.debugMode === true) {
+      console.log(state);
+    }
+  }
+
+  // runs when input is true and its submitted
+  function pushChat(e) {
+    e.preventDefault();
+
+    //We can feed in state.data instead of inputField and manually grabbing from DOM
+    function showRequest(text) {
+      let conversationDiv = document.getElementById("conversation");
+      let requestParagraph = document.createElement("P");
+      requestParagraph.className = "userRequest";
+      requestParagraph.appendChild(document.createTextNode(text));
+      let spacer = document.createElement("div");
+      spacer.className = "conversationSpacer";
+      spacer.appendChild(requestParagraph);
+      conversationDiv.appendChild(spacer);
+      conversationDiv.scrollTop = conversationDiv.scrollHeight;
+    }
+
+    function showResponse(lexResponse) {
+      let conversationDiv = document.getElementById("conversation");
+      let responseParagraph = document.createElement("P");
+      responseParagraph.className = "lexResponse";
+      if (lexResponse.message) {
+        responseParagraph.appendChild(
+          document.createTextNode(lexResponse.message)
+        );
+      }
+      if (lexResponse.dialogState === "ReadyForFulfillment") {
+        responseParagraph.appendChild(
+          document.createTextNode("Ready for fulfillment")
+        );
+      }
+      let spacer = document.createElement("div");
+      spacer.className = "conversationSpacer";
+      spacer.appendChild(responseParagraph);
+      conversationDiv.appendChild(spacer);
+      conversationDiv.scrollTop = conversationDiv.scrollHeight;
+    }
+
+    function showError(text) {
+      let conversationDiv = document.getElementById("conversation");
+      let errorParagraph = document.createElement("P");
+      errorParagraph.className = "lexError";
+      errorParagraph.appendChild(document.createTextNode(text));
+      let spacer = document.createElement("div");
+      spacer.className = "conversationSpacer";
+      spacer.appendChild(errorParagraph);
+      conversationDiv.appendChild(spacer);
+      conversationDiv.scrollTop = conversationDiv.scrollHeight;
+    }
+
+    //params needed for lex runtime to send to backend.
+    let params = {
+      botAlias: "$LATEST",
+      botName: props.botName,
+      inputText: state.data,
+      userId: state.lexUserId,
+      sessionAttributes: state.sessionAttributes,
+    };
+
+    /***** Can we use state.data to check value changes to signal the ... sending message ***/
+    let inputFieldText = document.getElementById("inputField");
 
     if (
       inputFieldText &&
       inputFieldText.value &&
       inputFieldText.value.trim().length > 0
     ) {
-      // disable input to show we're sending it
+      //disable input to show we're sending it
       var inputField = inputFieldText.value.trim();
       inputFieldText.value = "...";
       inputFieldText.locked = true;
-
-      // send it to the Lex runtime
-      var params = {
-        botAlias: "$LATEST",
-        botName: this.props.botName,
-        inputText: inputField,
-        userId: this.state.lexUserId,
-        sessionAttributes: this.state.sessionAttributes,
-      };
-      this.showRequest(inputField);
-      var a = function (err, data) {
-        if (err) {
-          console.log(err, err.stack);
-          this.showError(
-            "Error:  " + err.message + " (see console for details)"
-          );
-        }
-        if (data) {
-          // capture the sessionAttributes for the next cycle
-          this.setState({ sessionAttributes: data.sessionAttributes });
-          // show response and/or error/dialog status
-          this.showResponse(data);
-        }
-        // re-enable input
-        inputFieldText.value = "";
-        inputFieldText.locked = false;
-      };
-
-      this.lexruntime.postText(params, a.bind(this));
     }
+
+    /***** Can we use state.data to check value changes to signal the ... sending message ***/
+
+    //We can feed in state.data instead of inputField and manually grabbing from DOM
+    showRequest(inputField);
+
+    lexruntime.postText(params, function (err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        showError("Error:" + err.message + "(see console for detail)");
+      } else if (data) {
+        // capture the sessionAttributes for the next cycle
+
+        setState({ sessionAttributes: data.sessionAttributes });
+        // show response and/or error/dialog status
+        showResponse(data);
+      }
+      // re-enable input
+
+      inputFieldText.value = "";
+      inputFieldText.locked = false;
+    });
+
     // we always cancel form submission
     return false;
   }
 
-  showRequest(daText) {
-    var conversationDiv = document.getElementById("conversation");
-    var requestPara = document.createElement("P");
-    requestPara.className = "userRequest";
-    requestPara.appendChild(document.createTextNode(daText));
-    var spacer = document.createElement("div");
-    spacer.className = "convoSpacer";
-    spacer.appendChild(requestPara);
-    conversationDiv.appendChild(spacer);
-    conversationDiv.scrollTop = conversationDiv.scrollHeight;
-  }
+  const inputStyle = {
+    padding: "4px",
+    fontSize: 24,
+    width: "388px",
+    height: "40px",
+    borderRadius: "1px",
+    border: "10px",
+  };
 
-  showError(daText) {
-    var conversationDiv = document.getElementById("conversation");
-    var errorPara = document.createElement("P");
-    errorPara.className = "lexError";
-    errorPara.appendChild(document.createTextNode(daText));
-    var spacer = document.createElement("div");
-    spacer.className = "convoSpacer";
-    spacer.appendChild(errorPara);
-    conversationDiv.appendChild(spacer);
-    conversationDiv.scrollTop = conversationDiv.scrollHeight;
-  }
+  const conversationStyle = {
+    width: "400px",
+    height: props.height,
+    border: "px solid #ccc",
+    backgroundColor: props.backgroundColor,
+    padding: "4px",
+    overflow: "scroll",
+    borderBottom: "thin ridge #bfbfbf",
+  };
 
-  showResponse(lexResponse) {
-    var conversationDiv = document.getElementById("conversation");
-    var responsePara = document.createElement("P");
-    responsePara.className = "lexResponse";
-    if (lexResponse.message) {
-      responsePara.appendChild(document.createTextNode(lexResponse.message));
-    }
-    if (lexResponse.dialogState === "ReadyForFulfillment") {
-      responsePara.appendChild(
-        document.createTextNode("Ready for fulfillment")
-      );
-      // TODO:  show slot values
-    }
-    var spacer = document.createElement("div");
-    spacer.className = "convoSpacer";
-    spacer.appendChild(responsePara);
-    conversationDiv.appendChild(spacer);
-    conversationDiv.scrollTop = conversationDiv.scrollHeight;
-  }
+  const defaultHeaderRectStyle = {
+    backgroundColor: "#000000",
+    width: "408px",
+    minHeight: "40px",
+    textAlign: "center",
+    paddingTop: 12,
+    paddingBottom: 12,
+    display: "flex",
+    alignItems: "center",
+    color: "#FFFFFF",
+    fontSize: "24px",
+    justifyContent: "space-between",
+  };
 
-  handleChange(event) {
-    event.preventDefault();
-    this.setState({ data: event.target.value });
-  }
+  const headerReactStyle = merge(defaultHeaderRectStyle, props.headerStyle);
 
-  render() {
-    const inputStyle = {
-      padding: "4px",
-      fontSize: 24,
-      width: "388px",
-      height: "40px",
-      borderRadius: "1px",
-      border: "10px",
-    };
+  const chatcontainerStyle = {
+    backgroundColor: "#FFFFFF",
+    width: 408,
+  };
 
-    const conversationStyle = {
-      width: "400px",
-      height: this.props.height,
-      border: "px solid #ccc",
-      backgroundColor: this.props.backgroundColor,
-      padding: "4px",
-      overflow: "scroll",
-      borderBottom: "thin ridge #bfbfbf",
-    };
+  const chatFormStyle = {
+    margin: "1px",
+    padding: "2px",
+  };
 
-    const defaultHeaderRectStyle = {
-      backgroundColor: "#000000",
-      width: "408px",
-      minHeight: "40px",
-      textAlign: "center",
-      paddingTop: 12,
-      paddingBottom: 12,
-      display: "flex",
-      alignItems: "center",
-      color: "#FFFFFF",
-      fontSize: "24px",
-      justifyContent: "space-between",
-    };
-
-    const headerReactStyle = merge(
-      defaultHeaderRectStyle,
-      this.props.headerStyle
-    );
-
-    const chatcontainerStyle = {
-      backgroundColor: "#FFFFFF",
-      width: 408,
-    };
-
-    const chatFormStyle = {
-      margin: "1px",
-      padding: "2px",
-    };
-
-    return (
-      <div id="chatwrapper">
-        <div
-          id="chat-header-rect"
-          style={headerReactStyle}
-          onClick={this.handleClick}
+  return (
+    <div id="chatwrapper">
+      <div
+        id="chat-header-rect"
+        style={headerReactStyle}
+        onClick={handleClick} // change handleClick to handleClick()
+      >
+        <span />
+        <span
+          style={{
+            fontSize: headerReactStyle.fontSize,
+            color: headerReactStyle.color,
+          }}
         >
-          <span />
-          <span
-            style={{
-              fontSize: headerReactStyle.fontSize,
-              color: headerReactStyle.color,
-            }}
-          >
-            {this.props.headerText}
-          </span>
+          {props.headerText}
+        </span>
 
-          {this.state.visible === "open" ? (
-            <span className="chevron top"></span>
-          ) : (
-            <span className="chevron bottom"></span>
-          )}
-        </div>
-        <div
-          id="chatcontainer"
-          className={this.state.visible}
-          style={chatcontainerStyle}
-        >
-          <div
-            id="conversation"
-            ref={this.conversationDivRef}
-            style={conversationStyle}
-          />
-          <form
-            id="chatform"
-            style={chatFormStyle}
-            onSubmit={this.pushChat.bind(this)}
-          >
-            <input
-              type="text"
-              id="inputField"
-              size="40"
-              value={this.state.data}
-              placeholder={this.props.placeholder}
-              onChange={this.handleChange.bind(this)}
-              style={inputStyle}
-            />
-          </form>
-        </div>
+        {state.visible === "open" ? (
+          <span className="chevron top"></span>
+        ) : (
+          <span className="chevron bottom"></span>
+        )}
       </div>
-    );
-  }
+      <div
+        id="chatcontainer"
+        className={state.visible}
+        style={chatcontainerStyle}
+      >
+        <div
+          id="conversation"
+          ref={conversationDivRef}
+          style={conversationStyle}
+        >
+          <p className="lexResponse">{props.greeting}</p>
+        </div>
+        <form id="chatform" style={chatFormStyle} onSubmit={pushChat}>
+          <input
+            type="text"
+            id="inputField"
+            size="40"
+            value={state.data}
+            placeholder={props.placeholder}
+            onChange={handleChange} // changed the handleChange to handleChange()
+            style={inputStyle}
+            autoFocus={true} //added this autofocus equals true to remove focus
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
 
 LexChat.propTypes = {
@@ -278,6 +264,9 @@ LexChat.propTypes = {
   headerFontSize: PropTypes.number,
   sessionAttributes: PropTypes.object,
   debugMode: PropTypes.bool,
+  region: PropTypes.string,
+  greeting: PropTypes.string,
+  headerStyle: PropTypes.object,
 };
 
 LexChat.defaultProps = {
@@ -285,6 +274,7 @@ LexChat.defaultProps = {
   greeting: "",
   sessionAttributes: {},
   debugMode: false,
+  region: "us-east-1",
 };
 
 export default LexChat;
